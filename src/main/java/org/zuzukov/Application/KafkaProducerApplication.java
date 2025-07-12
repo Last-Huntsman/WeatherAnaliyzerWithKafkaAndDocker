0a51bee49ed558894aaae2030e6b2d9b78c2b216
Egor <zuzik17@yandex.ru>
2025-07-12 11:17:33 +0300

    Random generation weather,cities,temperature,timeweather,timestamp

--- src/main/java/org/zuzukov/Application/KafkaProducerApplication.java
+++ src/main/java/org/zuzukov/Application/KafkaProducerApplication.java
@@ -7,9 +7,11 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zuzukov.Enums.Cities;
 import org.zuzukov.Enums.Weathers;
 import org.zuzukov.Json.JsonSerializer;
 
+import java.time.LocalDateTime;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -18,6 +20,7 @@ public class KafkaProducerApplication {
     private static final Logger LOG = LoggerFactory.getLogger(KafkaProducerApplication.class);
     private static final Random RANDOM = new Random();
     private static  final Weathers[] weathers = Weathers.values();
+    private static  final Cities[] cities = Cities.values();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var properties = new Properties();
@@ -29,8 +32,15 @@ public class KafkaProducerApplication {
 
         try (var producer = new KafkaProducer<String, JSONObject>(properties)) {
             while (true) {
+
                 JSONObject jsObject = new JSONObject();
                 jsObject.put("weather", weathers[RANDOM.nextInt(weathers.length)]);
+                jsObject.put("cities", cities[RANDOM.nextInt(cities.length)]);
+                jsObject.put("temperature",RANDOM.nextInt(0,38));
+                jsObject.put("timeweather",)
+                jsObject.put("timestamp", LocalDateTime.now());
+
+                System.out.println(jsObject);
                 var metadata = producer.send(new ProducerRecord<>("weather", jsObject )).get();
 
                 LOG.info("====================");
